3bfc766ee13ca79e4dd3978b29415d8645ced538
junhc <451970658@qq.com>
2015-08-10 13:21:30 +0800

    页面细节处理

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var ckstatus = require('./routes/ckstatus');
+var check = require('./routes/check');
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'assets')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/tools/ckstatus', ckstatus);
+app.use('/tools/check', check);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/check.js
+++ routes/check.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('ckstatus', { title: 'Super Tools' });
+  res.render('check', { title: 'Super Tools' });
 });
 
 module.exports = router;
